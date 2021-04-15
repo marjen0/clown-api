da6f7aa508e58fba6a5819415a0c1484367835d2
marjen0 <m.jenulis@gmail.com>
2021-04-15 13:19:04 +0300

    cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://0.0.0.0:8080',
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
